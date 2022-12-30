7fa6872cf79c79fdfa7a52a36ef036e1f510bb6c
Lin <reven9919@gmail.com>
2022-12-30 01:51:01 -0800

    instatok

---
+++ 22fallswiftaa/InstaTok/InstaTok/Login/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  InstaTok
+//
+//  Created by Lin Liu on 11/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
