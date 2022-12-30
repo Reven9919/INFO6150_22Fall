3f7a666b4075e2e1fc57620a3682c9859d75717b
Lin <reven9919@gmail.com>
2022-12-30 01:42:25 -0800

    AlertControllerExample

---
+++ 22fallswiftaa/AlertControllerExample/AlertControllerExample/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  AlertControllerExample
+//
+//  Created by Lin Liu on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var txtField: UITextField?
+    @IBOutlet weak var lblAlert: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showalert(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Alert Controller Example", message: "Some Message", preferredStyle:  .alert)
+        let OKButton = UIAlertAction(title: "OK", style: .default){
+            action in
+            print("OK Button Pressed")
+            self.lblAlert.text = self.txtField?.text
+        }
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel){
+            action in
+            print("Cancel Button Pressed")
+            
+        }
+        
+        alertController.addAction(OKButton)
+        alertController.addAction(cancelButton)
+        
+        alertController.addTextField{ lblTextField in
+            self.txtField = lblTextField
+            lblTextField.placeholder = "Type something here"
+            
+        }
+        
+        self.present(alertController, animated: true)
+    }
+    
+}
+
